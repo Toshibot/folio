f6cf2268872db61b4e9c4b879c3bc377823c27e5
toshibot <toshi.stewart@gmail.com>
2024-01-14 22:54:16 +0100

    Added logo to the footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,12 @@
 <template>
-    <section>
-        <article>
-            <h1>Footer</h1>
-        </article>
-    </section>
+  <section class="footer-section">
+    <article>
+      <div class="footer-logo-container">
+        <img src="img/avatar.png"/>
+        <h2>Toshi Stewart</h2>
+      </div>
+    </article>
+  </section>
 </template>
 <script lang="ts">
 
@@ -15,5 +18,29 @@ export default {
 }
 </script>
 <style scoped lang="scss">
+  section.footer-section {
+    padding: 64px 0;
+    background: #181818;
+  }
+  article {
+    max-width: 1024px;
+    margin: 0 auto;
+    padding: 0 16px;
+  }
+  .footer-logo-container {
+    display: flex;
 
+    img {
+      height: 48px;
+      width: 48px;
+      margin-right: 16px;
+    }
+
+    h2 {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 48px;
+      font-weight: 700;
+      line-height: 1;
+    }
+  }
 </style>
